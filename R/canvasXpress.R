3ddd2b7da942b7b75dd9ff01bc2f066fa6fbbdd9
Dr. Connie Brett <connie@aggregate-genius.com>
2017-06-16 10:04:08 -0700

    simplification for rewrite

--- R/canvasXpress.R
+++ R/canvasXpress.R
@@ -16,188 +16,86 @@
 #' @aliases canvasXpress-package
 "_PACKAGE"
 
+
 #' canvasXpress
 #'
 #' Custom HTML widget creation function based on widget YAML and JavaScript for 
 #' use in any html-compatible context
 #'  
-#' @param data canvasXpress data frame
-#' \emph{(rows are refered to variables; columns are refered to samples)}
-#' @param decorData decoration/annotation data for the chart
-#' @param smpAnnot additional data that applies to samples (columns)
-#' @param varAnnot additional data that applies to variables (rows)
-#' @param nodeData network visualization data nodes
-#' @param edgeData network visualization data edges
-#' @param vennData venn visualization data
-#' @param vennLegend venn visualization legend
-#' @param genomeData genome visualization data
-#' @param newickData dendrogram data in Newick format
-#' @param graphType type of graph to be plotted - default = 'Scatter2D'
-#' @param events user-defined events (eg. mousemove, mouseout, click and dblclick)
-#' @param afterRender event triggered after rendering
-#' @param width plot width (valid CSS units) - default = 600px
-#' @param height plot height (valid CSS units) - default = 400px
-#' @param pretty print tagged code (json/html) nicely - default = FALSE
-#' @param digits display digits - default = 4
-#' @param ... additional configuration options passed to canvasXpress
-#' @param boxplotGroupData character label for grouped boxplot data.  If there is a 
-#' non-null value and graphType is 'Boxplot' pre-calculated boxplot data is used from
-#' the variables (iqr1, iqr3, qtl1, qtl3, median) and optionally the variable (outliers)
-#' in the data parameter.  Note: Boxplot outliers should be specified as 
-#' comma-separated string values in the outliers variable.
 #' 
 #' @return htmlwidget object
 #'
 #' @export
-canvasXpress <- function(data = NULL,       decorData = NULL, 
-                         smpAnnot = NULL,   varAnnot = NULL, 
-                         nodeData = NULL,   edgeData = NULL, 
-                         vennData = NULL,   vennLegend = NULL, 
-                         genomeData = NULL, newickData = NULL,
-                         graphType ='Scatter2D', events = NULL, afterRender=NULL, 
-                         width  = 600, height=400, pretty = FALSE, digits=4, ..., 
-                         boxplotGroupData = NULL) {
+canvasXpress <- function(data = NULL,     # y
+                         smpAnnot = NULL, # x
+                         varAnnot = NULL, # z
+                         #config items
+                         graphType = 'Scatter2D', 
+                         # straight-through
+                         events = NULL, 
+                         afterRender=NULL,
+                         #htmlwidgets options
+                         pretty = FALSE,
+                         digits = 4,
+                         width  = 600, 
+                         height = 400,
+                         ... ) {
     
-    assertCanvasXpressData(graphType, data, 
-                           nodeData, edgeData, 
-                           vennData, vennLegend, 
-                           genomeData, boxplotGroupData)
-    assertCanvasXpressDataFrame(graphType, data, 
-                                smpAnnot, varAnnot, 
-                                nodeData, edgeData, 
-                                vennData, vennLegend, 
-                                genomeData)
-    dataframe = "columns"
-    if (graphType == 'Network') {
-        nodes <- NULL
-        edges <- NULL
-        if (!is.null(nodeData)) {
-            nodes <- nodeData
-        }
-        if (!is.null(edgeData)) {
-            edges <- edgeData
-            if (is.null(nodeData)) {
-                nodes <- unique(c(
-                        as.vector(edgeData[,grep("id1", colnames(edgeData))]), 
-                        as.vector(edgeData[,grep("id2", colnames(edgeData))])))
-                names(nodes) <- rep("id", length(nodes))
-            }
-        }
-        dataframe = "rows"
-        data <- list(nodes = nodes, edges = edges)
-    } 
-    else if (graphType == 'Venn') {
-        dataframe = "columns"
-        data <- list(venn = list(data = vennData, legend = vennLegend))
-    } 
-    else if (graphType == 'Genome') {
-        #TBD
-    } 
-    else {
+    assertDataCorrectness(data, graphType)
+    
+        #build y
+    
         vars = as.list(assignCanvasXpressRownames(data))
         smps = as.list(assignCanvasXpressColnames(data))
-        
-        if (graphType == 'Boxplot' && !is.null(boxplotGroupData)) {
-            iqr1 = as.matrix(data["iqr1", ])
-            dimnames(iqr1) <- NULL
-            
-            iqr3 = as.matrix(data["iqr3" ,])
-            dimnames(iqr3) <- NULL
-            
-            median = as.matrix(data["median" ,])
-            dimnames(median) <- NULL
-            
-            qtl1 = as.matrix(data["qtl1" ,])
-            dimnames(qtl1) <- NULL
-            
-            qtl3 = as.matrix(data["qtl3" ,])
-            dimnames(qtl3) <- NULL
-            
-            y <- list(vars = as.list(boxplotGroupData),
-                      smps = smps,
-                      iqr1 = iqr1, 
-                      iqr3 = iqr3, 
-                      median = median,
-                      qtl1 = qtl1,
-                      qtl3 = qtl3
-            )
-            if ("outliers" %in% vars) {
-                out <- t(as.matrix(data["outliers", ]))
 
-                out.new <- sapply(out, strsplit, ",")
-                out.new <- unname(sapply(out.new, as.numeric))
-                out.new <- sapply(out.new, as.list)
-
-                y$out <- list(out.new)
-            }
-        } #boxplot with summarized data
-        else {
-            dy <- as.matrix(data)
-            dimnames(dy) <- NULL
-            y <- list(vars = vars, smps = smps, data = dy)
-        }
+        dy <- as.matrix(data, dimnames = list())
+        # dimnames(dy) <- NULL
+        y <- list(vars = vars, 
+                  smps = smps, 
+                  data = dy)
         
         x <- NULL
         z <- NULL
         if (!is.null(smpAnnot)) {
-            vars2 = as.list(assignCanvasXpressRownames(smpAnnot))
-            smps2 = as.list(assignCanvasXpressColnames(smpAnnot))
-            if (!identical(vars2, smps)) {
-                smpAnnot <- t(smpAnnot)
-                vars2 = as.list(assignCanvasXpressRownames(smpAnnot))
-                smps2 = as.list(assignCanvasXpressColnames(smpAnnot))
-            }
-            if (!identical(vars2, smps)) {
+            if (!identical(as.list(assignCanvasXpressColnames(smpAnnot)), smps)) {
                 stop("Column names in smpAnnot are different from column names in data")
             }
-            x <- lapply(convertRowsToList(t(smpAnnot)), function (d) if (length(d) > 1) d else list(d))
+            x <- lapply(convertRowsToList(smpAnnot), function(d) if (length(d) > 1) d else list(d))
         }
         if (!is.null(varAnnot)) {
-            vars3 = as.list(assignCanvasXpressRownames(varAnnot))
-            smps3 = as.list(assignCanvasXpressColnames(varAnnot))
-            if (!identical(vars3, vars)) {
+            if (!identical(as.list(assignCanvasXpressRownames(varAnnot)), vars)) {
                 stop("Row names in varAnnot are different from row names in data")
             }
-            z <- lapply(convertRowsToList(t(varAnnot)), function (d) if (length(d) > 1) d else list(d))
+            z <- lapply(convertRowsToList(t(varAnnot)), function(d) if (length(d) > 1) d else list(d))
         }
-        
-        data <- list(y = y, x = x, z = z)
-        
-        if (!is.null(decorData)) {
-            data[["d"]] <- decorData
-        } 
-        
-        if (!is.null(newickData)) {
-            data[["t"]] <- newickData
-        }
-    }
+            
+    # Data
+    data <- list(y = y, 
+                 x = x, 
+                 z = z)
+    
     
     # Config
-    config <- list(graphType = graphType, isR = TRUE, ...)
-    
-    # Events
-    # Nothing to do
-    
-    # After Render
-    # Nothing to do
+    config <- list(graphType = graphType, 
+                   ...)
+
     
     # CanvasXpress Object
-    cx = list(data = data, 
-              config = config, 
-              events = events, 
-              afterRender = afterRender)
+    cx_object <- list(data        = data, 
+                      config      = config, 
+                      events      = events, 
+                      afterRender = afterRender)
     
-    ## toJSON option
-    options(htmlwidgets.TOJSON_ARGS = list(dataframe = dataframe, 
-                                           pretty = pretty, 
-                                           digits = digits))
+    options(htmlwidgets.TOJSON_ARGS = list(dataframe = "columns", 
+                                           pretty    = pretty, 
+                                           digits    = digits))
     
-    # Create the widget
-    widget <- htmlwidgets::createWidget("canvasXpress", cx, 
-                              width = width, 
+    htmlwidgets::createWidget("canvasXpress", 
+                              cx_object, 
+                              width  = width,
                               height = height)
-    widget
 }
+
 
 
 #' canvasXpressOutput
@@ -220,6 +118,8 @@ canvasXpressOutput <- function(outputId, width = "100%", height = "400px") {
                                    width, height, 
                                    package = "canvasXpress")
 }
+
+
 
 #' renderCanvasXpress
 #'
--- R/internal_functionality.R
+++ R/internal_functionality.R
@@ -1,104 +1,29 @@
-
-assertCanvasXpressData <- function(graphType = 'Scatter2D',
-                                   data = NULL, 
-                                   nodeData = NULL, edgeData = NULL, 
-                                   vennData = NULL, vennLegend = NULL, 
-                                   genomeData = NULL, boxplotGroupData = NULL) {
+assertDataCorrectness <- function(data, graphType) {
+    if (is.null(data))      stop("data cannot be NULL!")
+    if (is.null(graphType)) stop("graphType cannot be NULL!")
     
-    if (graphType == 'Network') {
-        if (is.null(nodeData)) {
-            stop("Missing nodeData for Network visualization!")
-        }
-        if (is.null(edgeData)) {
-            stop("Missing edgeData for Network visualization!")
-        }
-    } else if (graphType == 'Venn') {
-        if (is.null(vennData)) {
-            stop("Missing data for Venn visualization")
-        }
-        if (is.null(vennLegend)) {
-            stop("Missing legend for Venn visualization")
-        }
-    } else if (graphType == 'Genome') {
-        if (is.null(genomeData)) {
-            stop("Missing data for Genome visualization")
-        }
-        stop("Not implemented yet!")
-    } 
-    else if (is.null(data)) {
-        stop("Missing canvasXpress data!")
-    }
-    else if (graphType == 'Boxplot' && !is.null(boxplotGroupData)) {
-        vars = as.list(assignCanvasXpressRownames(data))
-        
-        if (!("iqr1" %in% vars) || !("iqr3" %in% vars) ||
-            !("qtl1" %in% vars) || !("qtl3" %in% vars) ||
-            !("median" %in% vars)) {
-            stop('Incorrect Vars for Boxplot Group Data!\n', 'Must include: <iqr1, iqr3, qtl1, qtl3, median>')
-        }  
-    }
+    validTypes <- c("Bar", "Line", "Scatter2D", "Scatter3D", "Stacked")
+    
+    # validTypes <- c("Bar", "Line", "Area", "AreaLine", "BarLine", "Boxplot", 
+                    # "Dotplot", "DotLine", "Heatmap", "Candlestick", "Stacked", 
+                    # "StackedLine", "StackedPercent", "StackedPercentLine", 
+                    # "Tree", "Treemap", "TagCloud", "ParallelCoordinates", 
+                    # "Sankey", "Scatter2D", "ScatterBubble2D", "Scatter3D", 
+                    # "Correlation", "Pie", "Venn", "Network", "Genome", 
+                    # "Circular")
+
+    if (!(graphType %in% validTypes)) stop("graphType is invalid, must be one of <",
+                                           paste(validTypes, collapse =", "), ">")
+    
+    
+    
+    #Network
+    #Venn
+    #Genome
+    #Boxplot w/ precalc. flag
     
 }
 
-assertCanvasXpressDataFrame <- function(graphType = 'Scatter2D',
-                                        data = NULL, smpAnnot = NULL, varAnnot = NULL, 
-                                        nodeData = NULL, edgeData = NULL, 
-                                        vennData = NULL, vennLegend = NULL, 
-                                        genomeData = NULL) {
-    
-    if (graphType == 'Network') {
-        if (!is.null(nodeData) && !is.data.frame(nodeData) && !is.matrix(nodeData)) {
-            stop("nodeData must be a data frame or a matrix class object.")
-        }
-        else {
-            if (!"id" %in% colnames(nodeData)) {
-                stop("missing 'id' header in nodeData dataframe.")
-            }
-        }
-        if (!is.null(edgeData) && !is.data.frame(edgeData) && !is.matrix(edgeData)) {
-            stop("edgeData must be a data frame or a matrix class object.")
-        }
-        else {
-            if (!"id1" %in% colnames(edgeData)) {
-                stop("missing 'id1' header in edgeData dataframe.")
-            }
-            if (!"id2" %in% colnames(edgeData)) {
-                stop("missing 'id2' header in edgeData dataframe.")
-            }
-        }
-    } else if (graphType == 'Venn') {
-        if (!is.data.frame(vennData) && !is.matrix(vennData)) {
-            stop("vennData must be a data frame or a matrix class object.")
-        }
-        if (length(vennData) == 15) {
-            comp = c("A", "B", "C", "D", "AB", "AC", "AD", "BC", "BD", "CD", "ABC", "ABD", "ACD", "BCD", "ABCD")
-        } else if (length(vennData) == 7) {
-            comp = c("A", "B", "C", "AB", "AC", "BC", "ABC")
-        } else {
-            comp = c("A", "B", "AB")
-        }
-        for (c in comp) {
-            if (!c %in% colnames(vennData)) {
-                stop(cat("missing '", c, "' header in edgeData dataframe.", sep = ''))
-            }
-        }
-    } else if (graphType == 'Genome') {
-        if (!is.data.frame(genomeData) && !is.matrix(genomeData)) {
-            stop("genomeData must be a data frame or a matrix class object.")
-        }
-    } else {
-        if (!is.data.frame(data) && !is.matrix(data)) {
-            stop("data must be a data frame or a matrix class object.")
-        }
-        if (!is.null(smpAnnot) && !is.data.frame(smpAnnot) && !is.matrix(smpAnnot)) {
-            stop("smpAnnot must be a data frame or a matrix class object.")
-        }
-        if (!is.null(varAnnot) && !is.data.frame(varAnnot) && !is.matrix(varAnnot)) {
-            stop("varAnnot must be a data frame or a matrix class object.")
-        }
-    }
-    
-}
 
 assignCanvasXpressColnames <- function(x) {
     if (is.null(colnames(x))) {
@@ -107,6 +32,7 @@ assignCanvasXpressColnames <- function(x) {
         colnames(x)
     }
 }
+
 
 assignCanvasXpressRownames <- function(x) {
     if (is.null(rownames(x))) {
@@ -117,12 +43,12 @@ assignCanvasXpressRownames <- function(x) {
 }
 
 
-seq_row <- function(x) {
-    # From BBmisc
-    seq_len(nrow(x))
-}
-
 convertRowsToList <- function(x) {
+    seq_row <- function(x) {
+        # From BBmisc
+        seq_len(nrow(x))
+    }
+    
     # From BBmisc
     res = lapply(seq_row(x), function(i) stats::setNames(x[i,], NULL))
     stats::setNames(res, rownames(x))
