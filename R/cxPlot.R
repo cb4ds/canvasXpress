9ef74ccc5e577b8637048541b70d07983433fdf6
Dr. Connie Brett <connie@aggregate-genius.com>
2017-06-21 12:13:14 -0700

    cxplot infra

--- R/cxPlot.R
+++ R/cxPlot.R
@@ -8,12 +8,53 @@ magrittr::`%>%`
 #' 
 #' TBD
 #' 
-#' @return cxPlot object
+#' @return cxplot object
 #' 
 #' @export 
-cxPlot <- function(plot, ...) {
+cxPlot <- function(data, 
+                   smpAnnot = NULL, 
+                   varAnnot = NULL,
+                   graphType = "Scatter2D", ...) {
+    
+    plot <- structure(
+                list(data = data,
+                     smpAnnot = smpAnnot,
+                     varAnnot = varAnnot,
+                     graphType = graphType,
+                     parameters = ...
+                     ), 
+                class = c("cxplot"))
+    
+    stop('not implemented yet')
+    
+    plot
+}
+
+#' modPlot
+#' 
+#' TBD
+#' 
+#' @return cxplot object
+#' 
+#' @export 
+modPlot <- function(cxplot, ...) {
     stop('not implemented yet')
 }
 
 
 
+#' Reports whether x is a cxplot object
+is.cxplot <- function(x) inherits(x, "cxplot")
+
+
+
+#' Explicitly creates htmlwidgets object for output
+#'
+#' @param x plot to display
+#' 
+#' @return htmlwidgets object
+#' 
+#' @export
+print.cxplot <- function(x, ...) {
+    stop('not implemented yet')
+}
