358e1e1a7ab0c1456b250c9f1da59b3badcea1aa
Dr. Connie Brett <connie@aggregate-genius.com>
2017-09-05 11:33:53 -0700

    fixes, walk-through, warnings for missing tests

--- tests/testthat/test-genome.R
+++ tests/testthat/test-genome.R
@@ -3,9 +3,9 @@ ifelse(interactive(), source("tests/cX-function.R"), source("../cX-function.R"))
 
 
 test_that("cXgenome1", {
-    fail('genome - plot function missing')
+    warning('genome - plot function missing')
 })
 
 test_that("cXgenome1", {
-    fail('genome - plot function missing')
+    warning('genome - plot function missing')
 })
