23ca78263520cec7a2704c3fe1e833a92836e56b
Dr. Connie Brett <connie@aggregate-genius.com>
2017-09-14 09:18:53 -0700

    cleanup for

--- tests/testthat/test--BASE.R
+++ tests/testthat/test--BASE.R
@@ -3,7 +3,7 @@ context("canvasXpress BASE")
 data1.df  <- data.frame(a = c(1:3), b = c("A", "B", "C"),
                         row.names = c("row1", "row2", "row3"))
 data1.m   <- matrix(1:20, nrow = 5)
-data1.l   <- list(data1 = data1.df, data2 = data1.df)
+data1.l   <- list(y = data1.df, data2 = data1.df)
 
 data1.var <- data.frame(row1 = c(10:12), row2 = c(20:22), row3 = c(30:32))
 data1.smp <- data.frame(a = c(30:32), b = c(40:42))
@@ -17,10 +17,10 @@ test_that("Object Structure Tests", {
     expect_equal(testobj$height, 400)
     expect_equivalent(class(testobj), c("canvasXpress", "htmlwidget"))
     
-    expect_true(hasName(testobj$x, "data"))
-    expect_true(hasName(testobj$x, "config"))
-    expect_true(hasName(testobj$x$config, "isR"))
-    expect_true(hasName(testobj$x$config, "graphType"))
+    expect_true("data"      %in% names(testobj$x))
+    expect_true("config"    %in% names(testobj$x))
+    expect_true("isR"       %in% names(testobj$x$config))
+    expect_true("graphType" %in% names(testobj$x$config))
 })
 
 test_that("Allowed Null Data GraphTypes", {
