34f1c93a916cdc621168b373e0fb7e1b4eaab1ee
Dr. Connie Brett <connie@aggregate-genius.com>
2017-09-29 08:19:09 -0700

    added tests

--- tests/testthat/test-boxplot2.R
+++ tests/testthat/test-boxplot2.R
@@ -3,6 +3,7 @@ context("canvasXpress Charts - Boxplot")
 precalc.data <- data.frame(iqr1   = c(45,   7.3, 8),     qtl1     = c(109, 14.9, 4),
                            median = c(159, 20.1, 7.25),  qtl3     = c(249, 26.2, 11.3),
                            iqr3   = c(337, 40.6, 17.4),  outliers = c("", "44.5, 46", ""),
+                           type   = c("A", "B", "C"),
                            stringsAsFactors = F)
 
 precalc.data.l <- as.list(precalc.data)
@@ -12,7 +13,22 @@ colnames(precalc.data) <- c("Assault", "Rape", "Murder")
 
 
 test_that("precalculated boxplot - dataframe data", {
-    result <- canvasXpress(data                  =  precalc.data,
+    result <- canvasXpress(data                  = precalc.data,
+                           graphType             = "Boxplot",
+                           graphOrientation      = "vertical",
+                           smpLabelFontStyle     = "italic",
+                           smpLabelRotate        = 90,
+                           showLegend            = FALSE,
+                           title                 = "US Arrests by Type",
+                           titleScaleFontFactor  = 0.5)
+    
+    if (interactive()) { print(result) }
+    
+    expect_s3_class(result, "canvasXpress")
+    expect_s3_class(result, "htmlwidget")
+    
+    result <- canvasXpress(data                  = precalc.data,
+                           smpAnnot              = precalc.data["type", ],
                            graphType             = "Boxplot",
                            graphOrientation      = "vertical",
                            smpLabelFontStyle     = "italic",
@@ -42,4 +58,36 @@ test_that("precalculated boxplot - list data", {
     
     expect_s3_class(result, "canvasXpress")
     expect_s3_class(result, "htmlwidget")
+    
+    # with smpAnnot data
+    result <- canvasXpress(data                  = precalc.data.l,
+                           smpAnnot              = precalc.data["type",],
+                           graphType             = "Boxplot",
+                           graphOrientation      = "vertical",
+                           smpLabelFontStyle     = "italic",
+                           smpLabelRotate        = 90,
+                           showLegend            = FALSE,
+                           title                 = "US Arrests by Type",
+                           titleScaleFontFactor  = 0.5)
+    
+    if (interactive()) { print(result) }
+    
+    expect_s3_class(result, "canvasXpress")
+    expect_s3_class(result, "htmlwidget")
+    
+    # without smpAnnot
+    result <- canvasXpress(data                  =  precalc.data.l,
+                           graphType             = "Boxplot",
+                           graphOrientation      = "vertical",
+                           smpLabelFontStyle     = "italic",
+                           smpLabelRotate        = 90,
+                           showLegend            = FALSE,
+                           title                 = "US Arrests by Type",
+                           titleScaleFontFactor  = 0.5)
+    
+    if (interactive()) { print(result) }
+    
+    expect_s3_class(result, "canvasXpress")
+    expect_s3_class(result, "htmlwidget")
 })
+
