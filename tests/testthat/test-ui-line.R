ad8cfdcc4e5b29249753c15b5ce99acf425fd875
saranya-ag <saranyakaruppasamy@ip-172-16-12-101.ca-central-1.compute.internal>
2019-11-28 06:39:36 +0000

    Resolved conflicts

--- tests/testthat/test-ui-line.R
+++ tests/testthat/test-ui-line.R
@@ -3,7 +3,8 @@ context("canvasXpress Web Charts - Line")
 
 test_that("cXline1", {
     check_ui_test(cXline1())
-    warning("Hovering over a line doesn't select the line like in the web example until plot is resized")
+
+    warning('Hover effect may not be visible if plot is not resized')
 })
 
 test_that("cXline2", {
