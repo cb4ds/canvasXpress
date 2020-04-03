1ba075b41c112d58a7384b5e479d0855848ef5a5
saranya-ag <saranyakaruppasamy@ip-172-16-12-101.ca-central-1.compute.internal>
2020-04-03 11:26:56 +0000

    removed space

--- tests/testthat/test-ui-oncoprint.R
+++ tests/testthat/test-ui-oncoprint.R
@@ -18,5 +18,4 @@ test_that("cXoncoprint4", {
 
     warning("The values on Tooltips are different from web. In the R version, there are NaN values while the web has zeroes. ",
              "This is due to the differences in the underlying data")
-
 })
