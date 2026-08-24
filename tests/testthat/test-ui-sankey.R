bcee498d996cc98a097506adb9ea0a80bbc0ee3d
Mohammed Ali <mohammed@aggregate-genius.com>
2026-08-24 02:55:22 -0700

    - Added sankey new unit tests

--- tests/testthat/test-ui-sankey.R
+++ tests/testthat/test-ui-sankey.R
@@ -24,3 +24,55 @@ test_that("cXsankey5", {
 test_that("cXsankey6", {
     check_ui_test(cXsankey6())
 })
+
+test_that("cXsankey7", {
+    check_ui_test(cXsankey7())
+})
+
+test_that("cXsankey8", {
+    check_ui_test(cXsankey8())
+})
+
+test_that("cXsankey9", {
+    check_ui_test(cXsankey9())
+})
+
+test_that("cXsankey10", {
+    check_ui_test(cXsankey10())
+})
+
+test_that("cXsankey11", {
+    check_ui_test(cXsankey11())
+})
+
+test_that("cXsankey12", {
+    check_ui_test(cXsankey12())
+})
+
+test_that("cXsankey13", {
+    check_ui_test(cXsankey13())
+})
+
+test_that("cXsankey14", {
+    check_ui_test(cXsankey14())
+})
+
+test_that("cXsankey15", {
+    check_ui_test(cXsankey15())
+})
+
+test_that("cXsankey16", {
+    check_ui_test(cXsankey16())
+})
+
+test_that("cXsankey17", {
+    check_ui_test(cXsankey17())
+})
+
+test_that("cXsankey18", {
+    check_ui_test(cXsankey18())
+})
+
+test_that("cXsankey19", {
+    check_ui_test(cXsankey19())
+})
